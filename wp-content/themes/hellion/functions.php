d64ef4b499c47cc7e748be44de372ef89004a23f
theblueskeleton <danielyordanov@hotmail.com>
2024-12-04 16:16:33 +0200

    Add theme screenshot file and enqueue the stylesheet

---
+++ wp-content/themes/hellion/functions.php
@@ -0,0 +1,6 @@
+<?php
+function zerofour_enqueue_styles() {
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/assets/css/main.css');
+}
+add_action('wp_enqueue_scripts', 'zerofour_enqueue_styles');
+?>
