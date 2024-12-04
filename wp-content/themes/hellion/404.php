1fde7fae6d79f87d6599d8c798576d0b3cf85586
theblueskeleton <danielyordanov@hotmail.com>
2024-12-04 19:04:50 +0200

    Create a 404 error page

---
+++ wp-content/themes/hellion/404.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<p style="color:red; font-size:30px; font-weight:bold; text-transform:capitalize; text-align:center;">404 File Not Found - Try checking your directory paths</p>
+
+<?php get_footer(); ?>
