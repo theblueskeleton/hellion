19db0f87a79546cc2359520e58ff352eb7e633d0
theblueskeleton <danielyordanov@hotmail.com>
2024-12-10 23:26:00 +0200

    Move banner into separate template part

--- wp-content/themes/hellion/index.php
+++ wp-content/themes/hellion/index.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
 
-				<!-- Banner 
-						<div id="banner">
-								<h2><strong>Hellion:</strong> A free responsive site template
-								<br />
-								built on HTML5 and CSS3 by <a href="http://html5up.net">HTML5 UP</a></h2>
-								<p>Does this statement make you want to click the big shiny button?</p>
-								<a href="#" class="button large icon solid fa-check-circle">Yes it does</a>
-							</div>-->
-
-					</div>
-				</div>
-
 			<!-- Main Wrapper -->
 				<div id="main-wrapper">
 					<div class="wrapper style1">
---
+++ wp-content/themes/hellion/template-parts/banner.php
@@ -0,0 +1,10 @@
+                    <!-- Banner -->
+                    <div id="banner">
+                        <h2><strong>Hellion:</strong> A free responsive site template
+                            <br />
+                            built on HTML5 and CSS3 by <a href="http://html5up.net">HTML5 UP</a>
+                        </h2>
+                        <p>Does this statement make you want to click the big shiny button and find the secret page?</p>
+                        <a href="<?php echo get_permalink(get_page_by_path('bazinga')); ?>" class="button large icon solid fa-check-circle">Yes it does</a>
+                    </div>
+                    </div>
